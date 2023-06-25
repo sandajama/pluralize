4b90ce35abbc0d70753f6d414397a45305bb70b5
sandajama <sandajama29@gmail.com>
2023-06-25 17:57:48 +0300

    answered

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 // Good Luck 💪🏾
+function pluralize(arr) {
+  let count = {}; 
+  let result = []; 
+for (let i = 0; i < arr.length; i++) {
+    let word = arr[i];
+    count[word] = (count[word] || 0) + 1; 
+  }
+for (let word in count) {
+    if (count[word] > 1) {
+      result.push(word + "s");
+    } else {
+      result.push(word);
+    }
+  }
+
+return result;
+}
+
+  console.log(pluralize(["cat","dog","cat","mouse"]))// ➞ ["cats","dog","mouse"]
+  console.log(pluralize(["car", "car", "car"])) //➞ ["cars"]
